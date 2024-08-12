c92cde84f3b310839abdb6e025e6d5d60eaa0435
unknown <ehddnr0167@naver.com>
2024-08-12 17:49:32 +0900

    생성된 게시물 정보를 Ariticle 클래스 객체로 저장 후 출력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -24,7 +24,14 @@ public class Main {
 
         int id = ++lastAricleId;
 
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        Article article = new Article(); // 게시물 객체 생성
+        article.id = id;
+        article.subject = subject;
+        article.content = content;
+
+        System.out.println("생성 된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+
       } else if (cmd.equals("exit")) {
         break;
       } else {
@@ -34,4 +41,10 @@ public class Main {
 
     System.out.println("== 자바 텍스트 게시판 종료 ==");
   }
+}
+
+class Article{
+  int id;
+  String subject;
+  String content;
 }
