53333a9e1385905153af998fa717a54b75cd5140
unknown <ehddnr0167@naver.com>
2024-08-13 17:16:47 +0900

    게시물 상세정보 출력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,31 +1,49 @@
 package com.sbs.java.board;
+
 import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+
     int lastArticleId = 0;
+    Article lastArticle = null;
+
     Scanner sc = new Scanner(System.in);
     System.out.println("== 자바 텍스트 게시판 ==");
-    while(true) {
+    while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
-      if(cmd.equals("/usr/article/write")) {
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
         System.out.print("내용 : ");
         String content = sc.nextLine();
+
         int id = ++lastArticleId;
-        Article article = new Article(); // 게시물 객체 생성
-        article.id = id;
-        article.subject = subject;
-        article.content = content;
+
+        Article article = new Article(id, subject, content); // 게시물 객체 생성
+        lastArticle = article;
+
         System.out.println("생성 된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      } else if (cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+
+        if (article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.subject);
+        System.out.printf("내용 : %s\n", article.content);
+
       }
-      else if(cmd.equals("exit")) {
+        else if (cmd.equals("exit")) {
         break;
-      }
-      else {
+      } else {
         System.out.println("잘못 된 명령어입니다.");
       }
     }
@@ -33,10 +51,17 @@ public class Main {
     sc.close();
   }
 }
+
 class Article {
   int id;
   String subject;
   String content;
+
+  Article(int id, String subject, String content) {
+    this.id = id;
+    this.subject = subject;
+    this.content = content;
+  }
 
   @Override // 어노테이션
   public String toString() { // 메서드 오버라이딩
