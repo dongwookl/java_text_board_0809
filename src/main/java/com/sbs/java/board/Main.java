a574041331bf095e5a80f18f30770f17134a0818
unknown <ehddnr0167@naver.com>
2024-08-13 17:36:16 +0900

    게시물 리스트를 최순순으로 변경

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -53,7 +53,7 @@ public class Main {
         System.out.println("-------------------");
         System.out.println("|  번호  |  제목  |");
         System.out.println("-------------------");
-        for(int i = 0; i < articles.size(); i++) {
+        for(int i = articles.size() - 1; i >= 0; i--) {
           Article article = articles.get(i);
           System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
         }
