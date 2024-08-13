890d3781f12bc45a280bad076f397e517099df8a
unknown <ehddnr0167@naver.com>
2024-08-13 17:34:44 +0900

    게시물 테스트 데이터 함수 도입

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,24 +1,25 @@
 package com.sbs.java.board;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  static void makeTestData(List<Article> articles) {
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+  }
+
   public static void main(String[] args) {
     int lastArticleId = 0;
     Article lastArticle = null;
 
     List<Article> articles = new ArrayList<>();
 
-    // 테스트 게시물
-    articles.add(new Article(1, "제목1", "내용1"));
-    articles.add(new Article(2, "제목2", "내용2"));
-    articles.add(new Article(3, "제목3", "내용3"));
+    makeTestData(articles);
 
     Scanner sc = new Scanner(System.in);
     System.out.println("== 자바 텍스트 게시판 ==");
-
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
@@ -43,23 +44,19 @@ public class Main {
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
-
       } else if (cmd.equals("/usr/article/list")) {
         if(articles.size() == 0) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
-
         System.out.println("== 게시물 리스트 ==");
         System.out.println("-------------------");
         System.out.println("|  번호  |  제목  |");
         System.out.println("-------------------");
-
         for(int i = 0; i < articles.size(); i++) {
           Article article = articles.get(i);
           System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
         }
-
       } else if (cmd.equals("exit")) {
         break;
       } else {
