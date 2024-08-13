3b8ebf7abfbe112d25e005373bf0967c443bfc76
unknown <ehddnr0167@naver.com>
2024-08-13 17:49:46 +0900

    lastArticle 필요성 제거

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -8,20 +8,17 @@ public class Main {
     articles.add(new Article(2, "제목2", "내용2"));
     articles.add(new Article(3, "제목3", "내용3"));
   }
+
   public static void main(String[] args) {
     int lastArticleId = 0;
-    Article lastArticle = null;
     List<Article> articles = new ArrayList<>();
 
     makeTestData(articles);
-
     if (!articles.isEmpty()) {
       lastArticleId = articles.get(articles.size() - 1).id;
     }
-
     Scanner sc = new Scanner(System.in);
     System.out.println("== 자바 텍스트 게시판 ==");
-
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
@@ -32,27 +29,27 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
         int id = ++lastArticleId;
+
         Article article = new Article(id, subject, content); // 게시물 객체 생성
-        lastArticle = article;
 
         articles.add(article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else if (cmd.equals("/usr/article/detail")) {
-
-        Article article = lastArticle;
-
-        if (article == null) {
+        if (articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
+
+        Article article = articles.get(articles.size() - 1);
+
         System.out.println("== 게시물 상세보기 ==");
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
 
       } else if (cmd.equals("/usr/article/list")) {
-        if (articles.size() == 0) {
+        if (articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
@@ -60,7 +57,6 @@ public class Main {
         System.out.println("-------------------");
         System.out.println("|  번호  |  제목  |");
         System.out.println("-------------------");
-
         for (int i = articles.size() - 1; i >= 0; i--) {
           Article article = articles.get(i);
           System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
