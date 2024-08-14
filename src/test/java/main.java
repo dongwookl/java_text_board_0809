c247a505ab711a69960f7434b2ff49072223cc7b
unknown <ehddnr0167@naver.com>
2024-08-14 16:34:16 +0900

    쪼개진 문장을 = 을 기준으로 한번 더 분리

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,18 +1,17 @@
 import java.util.Arrays;
-import java.util.SimpleTimeZone;
-
 public class main {
   public static void main(String[] args) {
     String queryString = "a=1&b=2&c=3";
     String[] queryStringBits = queryString.split("&");
 
     System.out.println(Arrays.toString(queryStringBits));
-  /*
-    for(String bit : queryStringBits) {
-      System.out.println(bit);
-    }
-    */
 
-    Arrays.stream(queryStringBits).forEach(System.out::println);
+    for(String bit : queryStringBits) {
+      String[] bitBits = bit.split("=");
+      String paramName = bitBits[0];
+      String paramValue = bitBits[1];
+
+      System.out.printf("%s : %s\n", paramName, paramValue);
+    }
   }
 }
