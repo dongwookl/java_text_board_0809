14e36318b8ec6dd6a69cda6b6bda35fbecaed05d
unknown <ehddnr0167@naver.com>
2024-08-14 17:07:34 +0900

    map를 이용하여 파라미터 분석하여 데이터 저장

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,27 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class main {
   public static void main(String[] args) {
-    String queryString = "hit=73&id=3&memberId=13&boardId=2";
+    String queryString = "id=3&memberId=13&boardId=2&hit=73&writerName=신짱구";
+
+    Map<String, String> params = new LinkedHashMap<>();
+    // LinkedHashMap : 순서를 보장
 
     // 압축해제 시작
     String[] queryStringBits = queryString.split("&");
 
-    List<String> paramNames = new ArrayList<>();
-    List<Integer> paramValues = new ArrayList<>();
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
-      String paramName = bitBits[0];
-      String paramValue = bitBits[1];
-      paramNames.add(paramName);
-      paramValues.add(Integer.parseInt(paramValue));
+      params.put(bitBits[0], bitBits[1]);
     }
     // 압축해제 끝
 
-    System.out.println(paramNames);
-    int boardIdIndex = paramNames.indexOf("boardId");
-    System.out.println("boardId : " + paramValues.get(boardIdIndex));
+    System.out.println("== 원하는 것을 하나하나 가져와서 사용 ==");
+    System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
+    System.out.printf("memberId : %d\n", Integer.parseInt(params.get("memberId")));
+    System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
+    System.out.printf("hit : %d\n", Integer.parseInt(params.get("hit")));
+    System.out.printf("writerName : %s\n", params.get("writerName"));
 
+    System.out.println("== 반복문을 이용한 순회 출력 ==");
+    for(String paramName : params.keySet()) {
+      String paramValue = params.get(paramName);
+
+      System.out.printf("%s : %s\n", paramName, paramValue);
+    }
   }
 }
