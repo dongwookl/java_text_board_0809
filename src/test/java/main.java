35694680459e3013df4ce49e39600aac32f5e9bf
unknown <ehddnr0167@naver.com>
2024-08-14 16:35:37 +0900

    쪼개진 파라벨류를 변수에 할당

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,17 +1,35 @@
 import java.util.Arrays;
+
 public class main {
   public static void main(String[] args) {
-    String queryString = "a=1&b=2&c=3";
+    String queryString = "a=100&b=20&c=30";
     String[] queryStringBits = queryString.split("&");
 
-    System.out.println(Arrays.toString(queryStringBits));
+    int a = 0;
+    int b = 0;
+    int c = 0;
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
       String paramName = bitBits[0];
       String paramValue = bitBits[1];
 
-      System.out.printf("%s : %s\n", paramName, paramValue);
+      if(paramName.equals("a")) {
+        a = Integer.parseInt(paramValue);
+      }
+      else if(paramName.equals("b")) {
+        b = Integer.parseInt(paramValue);
+      }
+      else if(paramName.equals("c")) {
+        c = Integer.parseInt(paramValue);
+      }
+
+      // System.out.printf("%s : %s\n", paramName, paramValue);
     }
+
+    System.out.printf("a : %d\n", a);
+    System.out.printf("b : %d\n", b);
+    System.out.printf("c : %d\n", c);
+
   }
 }
