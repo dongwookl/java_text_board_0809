6476ae34b215dfc923772e31d77a5dd50b7d5544
unknown <ehddnr0167@naver.com>
2024-08-19 17:24:51 +0900

    orberBy=idDesc 파라미터에 의한 정렬 방법 변경

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -28,41 +28,62 @@ public class Main {
         int id = ++lastArticleId;
         Article article = new Article(id, subject, content); // 게시물 객체 생성
         articles.add(article);
-
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else if (rq.getUrlPath().equals("/usr/article/detail")) {
         Map<String, String> params = rq.getParams();
-        int id = Integer.parseInt(params.get("id"));
-
+        int id = 0;
+        try {
+          id = Integer.parseInt(params.get("id"));
+        } catch (NumberFormatException e) {
+          System.out.println("id를 정수 형태로 입력해주세요.");
+          continue;
+        }
         if (articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        if(id > articles.size()) {
+        if (id > articles.size()) {
           System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
           continue;
         }
-
         Article article = articles.get(id - 1);
-
         System.out.println("== 게시물 상세보기 ==");
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
+
       } else if (rq.getUrlPath().equals("/usr/article/list")) {
-        if (articles.isEmpty()) {
-          System.out.println("게시물이 존재하지 않습니다.");
-          continue;
+        Map<String, String> params = rq.getParams();
+
+        boolean orderByIdDesc = true;
+
+        if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+          orderByIdDesc = false;
         }
+
         System.out.println("== 게시물 리스트 ==");
         System.out.println("-------------------");
         System.out.println("|  번호  |  제목  |");
         System.out.println("-------------------");
-        for (int i = articles.size() - 1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
+
+        if(orderByIdDesc) { // idAsc(오름차순)가 없으면 기본값인 idDesc(내림차순)
+          for (int i = articles.size() - 1; i >= 0; i--) {
+            Article article = articles.get(i);
+            System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
+          }
         }
+        else {
+          for(Article article : articles) {
+            System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
+          }
+        }
+
+        if (articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
       } else if (rq.getUrlPath().equals("exit")) {
         break;
       } else {
@@ -107,15 +128,19 @@ class Util {
   static Map<String, String> getParamsFromUrl(String url) {
     Map<String, String> params = new LinkedHashMap<>();
     String[] urlBits = url.split("\\?", 2);
-    if(urlBits.length == 1) {
+
+    if (urlBits.length == 1) {
       return params;
     }
+
     String queryStr = urlBits[1];
-    for(String bit : queryStr.split("&")) {
+    for (String bit : queryStr.split("&")) {
       String[] bits = bit.split("=", 2);
-      if(bits.length == 1) {
+
+      if (bits.length == 1) {
         continue;
       }
+
       params.put(bits[0], bits[1]);
     }
     return params;
