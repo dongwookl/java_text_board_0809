9c7bbd45edd6f6f7ba372864f8ff2d59d01a8270
unknown <ehddnr0167@naver.com>
2024-08-22 16:52:27 +0900

    usr/article/list를 별도의 메서드로 분리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 public class Main {
   static void makeTestData(List<Article> articles) {
     IntStream.rangeClosed(1, 100)
-        .forEach(i -> articles.add(new Article(i, "제목" + i, "내용1" + i)));
+        .forEach(i -> articles.add(new Article(i, "제목" + i, "내용" + i)));
   }
 
   public static void main(String[] args) {
@@ -52,56 +52,68 @@ public class Main {
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
+
       } else if (rq.getUrlPath().equals("/usr/article/list")) {
-        Map<String, String> params = rq.getParams();
-        if (articles.isEmpty()) {
-          System.out.println("게시물이 존재하지 않습니다.");
-          continue;
-        }
-        boolean orderByIdDesc = true;
-        if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
-          orderByIdDesc = false;
-        }
-        // 검색 시작
-        List<Article> filteredArticles = articles;
-
-        if (params.containsKey("searchKeyword")) {
-          String searchKeyword = params.get("searchKeyword");
-
-          filteredArticles = new ArrayList<>();
-
-          for (Article article : articles) {
-            if (article.subject.contains(searchKeyword) || article.content.contains(searchKeyword)) {
-              filteredArticles.add(article);
-            }
-          }
-        }
-        // 검색 끝
-        List<Article> soredArticles = filteredArticles;
-        // List<Article> soredArticles = articles;
-        System.out.println("== 게시물 리스트 ==");
-        System.out.println("-------------------");
-        System.out.println("|  번호  |  제목  |");
-        System.out.println("-------------------");
-
-        if (orderByIdDesc) { // idAsc(오름차순)가 없으면 기본값인 idDesc(내림차순)
-          soredArticles = Util.reverseList(soredArticles);
-        }
-
-        for (Article article : soredArticles) {
-          System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
-        }
-
+        actionUsrArticleList(rq, articles);
       } else if (rq.getUrlPath().equals("exit")) {
         break;
       } else {
         System.out.println("잘못 된 명령어입니다.");
       }
     }
+
     System.out.println("== 자바 텍스트 게시판 종료 ==");
     sc.close();
   }
+
+  static void actionUsrArticleList(Rq rq, List<Article> articles) {
+    Map<String, String> params = rq.getParams();
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    boolean orderByIdDesc = true;
+
+    if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+      orderByIdDesc = false;
+    }
+
+    // 검색 시작
+    List<Article> filteredArticles = articles;
+
+    if (params.containsKey("searchKeyword")) {
+      String searchKeyword = params.get("searchKeyword");
+
+      filteredArticles = new ArrayList<>();
+
+      for (Article article : articles) {
+        if (article.subject.contains(searchKeyword) || article.content.contains(searchKeyword)) {
+          filteredArticles.add(article);
+        }
+      }
+    }
+    // 검색 끝
+
+    List<Article> soredArticles = filteredArticles;
+    // List<Article> soredArticles = articles;
+
+    System.out.println("== 게시물 리스트 ==");
+    System.out.println("-------------------");
+    System.out.println("|  번호  |  제목  |");
+    System.out.println("-------------------");
+
+    if (orderByIdDesc) { // idAsc(오름차순)가 없으면 기본값인 idDesc(내림차순)
+      soredArticles = Util.reverseList(soredArticles);
+    }
+
+    for (Article article : soredArticles) {
+      System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
+    }
+  }
 }
+
 class Article {
   int id;
   String subject;
@@ -152,12 +164,10 @@ class Util {
   static String getUrlPathFromUrl(String url) {
     return url.split("\\?", 2)[0];
   }
-
   // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다.
   // 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
   public static <T> List<T> reverseList(List<T> list) {
     List<T> reverse = new ArrayList<>(list.size());
-
     for (int i = list.size() - 1; i >= 0; i--) {
       reverse.add(list.get(i));
     }
